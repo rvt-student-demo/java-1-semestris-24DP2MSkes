75d64d06e20b4e1f467ae118d6b6b6d99e1fb23e
Marta Šķestere <A240399MS@rvt.lv>
2025-09-24 12:24:11 +0300

    sazarojumi

---
+++ src/main/java/rvt/sazarojumi.java
@@ -0,0 +1,20 @@
+package rvt;
+import java.util.Scanner;
+
+public class sazarojumi {
+
+
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+int speed = Integer.valueOf(scanner.nextLine());
+if (speed > 120) {
+System.out.println("Speeding ticket!");
+} else {
+System.out.println("no speeding ticket");
+}
+scanner.close();
+}
+}
+
