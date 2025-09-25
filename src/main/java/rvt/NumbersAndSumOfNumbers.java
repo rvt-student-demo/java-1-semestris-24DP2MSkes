642d16a55e54c1aa262f2d4e4c7bea88ceb67afa
Marta Šķestere <A240399MS@rvt.lv>
2025-09-25 09:13:43 +0300

    refactor NumbersAndSumOfNumbers to accept user input and calculate sum dynamically

--- src/main/java/rvt/NumbersAndSumOfNumbers.java
+++ src/main/java/rvt/NumbersAndSumOfNumbers.java
@@ -3,12 +3,21 @@ import java.util.Scanner;
 public class NumbersAndSumOfNumbers {
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int NumberCount = 0;
+    int numberCount = 0;
     int sum = 0;
     while (true) {
-        NumberCount++;
-        sum += NumberCount;    
-    }   
+        System.out.println("Give a Number: ");
+        int number = scanner.nextInt();
+        if (number == 0) {
+            break;
+        }
+        numberCount++;
+        sum += number;   
+    } 
+     System.out.println("Number of numbers: " + numberCount); 
+        System.out.println("Sum of numbers: " + sum);
+
+    scanner.close();
 }
     
 }
