54ac29558486e4f5f20baa56302f44be091fef7e
Marta Šķestere <A240399MS@rvt.lv>
2025-09-25 09:07:15 +0300

    add NumbersAndSumOfNumbers class to calculate the sum of consecutive integers

---
+++ src/main/java/rvt/NumbersAndSumOfNumbers.java
@@ -0,0 +1,14 @@
+package rvt;
+import java.util.Scanner;
+public class NumbersAndSumOfNumbers {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int NumberCount = 0;
+    int sum = 0;
+    while (true) {
+        NumberCount++;
+        sum += NumberCount;    
+    }   
+}
+    
+}
